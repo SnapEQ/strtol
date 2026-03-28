489ec0f7a317af9da13676e030408facc133b4aa
SnapEQ <woo.biink@gmail.com>
2026-03-28 14:39:38 +0100

    Fixed version, changed architecture

--- strtol.c
+++ strtol.c
@@ -1,89 +1,123 @@
 /* dummy implementation of strtol */
-#include <stdlib.h>
-#include <stdio.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <string.h>
-
-char *getValidBases(const char* bases, int base);
-bool isValidChar(char c, const char* validBases, int base);
-
+#include <errno.h>
+#include <ctype.h>
 
 long int strtol(const char* nPtr, char** endPtr, int base)
 {
+    const char *s = nPtr;
+    bool isNegative = false;
+    unsigned long cutoff;
+    int any = 0;
+    unsigned long acc = 0;
+    int cutlim;
     if (endPtr) {
         *endPtr = (char*)nPtr;
     }
 
-    if ((base < 2 || base > 36) && base != 0)
+    if (base != 0 && (base < 2 || base > 36))
     {
+        errno = EINVAL;
         return 0;
     }
 
-    bool isAutoDetect = false;
+    while(isspace((unsigned char)*s))
+    {
+        s++;
+    }
+
+    if (*s == '-')
+    {
+        isNegative = true;
+        s++;
+    } else if (*s == '+') {
+        s++;
+    }
 
     if (base == 0)
     {
-        isAutoDetect = true;
-    }
-
-    bool isOctal = false;
-    bool isHex = false;
-
-    if (strlen(nPtr) > 2 || isAutoDetect)
-    {
-        if (nPtr[0] == '0')
+        if (*s == '0')
         {
-            if (nPtr[1] == 'x' || nPtr[1] == 'X'){
-                isHex = true;
+            if ((s[1] == 'x' || s[1] == 'X') && isxdigit((unsigned char)s[2])) 
+            {
                 base = 16;
-
+                s+=2;
             } else {
-                isOctal = true;
                 base = 8;
             }
-
+        } else {
+            base = 10;
+        }
+    } else if (base ==  16) {
+        if (s[0] == '0' && (s[1] == 'x' || s[1] == 'X') &&
+            isxdigit((unsigned char)s[2])) {
+            s+=2;
         }
     }
 
-    const char bases[]="0123456789ABDCEFGHIJKLMNOPQRSTUVWXYZ";
+    cutoff = isNegative ? -(unsigned long)LONG_MIN : LONG_MAX;
+    cutlim = (int)(cutoff % (unsigned long)base);
+    cutoff /= (unsigned long)base; 
 
-    char *validBases = getValidBases(bases, base);
+    for(; *s; s++)
+    {
+        int c = (unsigned char)*s;
+        int digit;
 
-    if (validBases == NULL){
+        if (c >= '0' && c <= '9')
+        {
+            digit = c - '0';
+        } else if (c >= 'A' && c <= 'Z') {
+            digit = c - 'A' + 10;
+        } else if (c >= 'a' && c <= 'z') {
+            digit = c - 'a' + 10;
+        } else {
+            break;
+        }
+
+        if (digit >= base){
+            break;
+        }
+
+        if (any < 0) {
+            continue;
+        }
+
+        if (acc > cutoff || (acc == cutoff && digit > cutlim)){
+            any = -1;
+            acc = cutoff;
+            errno = ERANGE;
+        } else {
+            any = 1;
+            acc = acc * (unsigned long)base + (unsigned long)digit;
+        }
+    }
+
+    if (any == 0)
+    {
+        if (endPtr)
+        {
+            *endPtr = (char *)nPtr;
+        }
+        errno = EINVAL;
         return 0;
     }
- 
 
-    free(validBases);
-    return 0;
-}
-
-char *getValidBases(const char* bases, int base)
-{
-    char *result = malloc(base * sizeof(char));
-
-    if(result == NULL){
-        fprintf(stderr, "Failure to allocate memory");
-        return NULL;
-    }
-
-    for (int i = 0; i < base; i++)
+    if (endPtr)
     {
-        result[i] = bases[i];
+        *endPtr = (char *)s;
     }
 
-    return result;
-}
-
-bool isValidChar(const char c, const char* validBases, int base)
-{
-    for(int i = 0; i < base; i++)
+    if (any < 0) 
     {
-        if(c == validBases[i])
-        {
-            return true;
-        }
+        return isNegative ? LONG_MIN : LONG_MAX;
     }
 
-    return false;
+    if (isNegative){
+        return -(long)acc;
+    }
+
+    return (long)acc;
 }
